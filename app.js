a68494b4ac7e62017691e31963bc613fa886c5c6
Pratik Dash <dashpratik40@gmail.com>
2022-08-31 23:28:41 +0530

    Added CSS styling

--- app.js
+++ app.js
@@ -11,31 +11,25 @@ button.addEventListener("click", ()=>{
         
 
         var init = Number(initialPrice.value)
-        initialPrice.style.border = "1px solid green"
+        
         var qty = Number(quantity.value)
-        quantity.style.border = "1px solid green"
+        
         var current = Number(currentPrice.value)
-        currentPrice.style.border = "1px solid green"
+       
 
         if(init>0 && qty>0 && current >0){
         checkProfitOrLoss(init,qty,current)
         }else{
-            output.innerText = "please provide valid details"
+            output.innerText = "Please provide valid details. The values must be greater than 0"
             output.style.color = "red"
+            
         }
 }
 else{
    
-        if(initialPrice.value === ''){
-            initialPrice.style.border = "1px solid red"
-        }
-        if(quantity.value === ''){
-            quantity.style.border = "1px solid red"
-        }
-        if(currentPrice.value === ''){
-           currentPrice.style.border = "1px solid red"
-        }
+    
         output.innerText = 'One or more input fields are empty. Please fill all the required details.'
+        output.className = "out-loss"
     
 }
 })
@@ -48,17 +42,21 @@ function checkProfitOrLoss(init,qty,current){
     
     if(totalInitPrice>totalCurrentPrice)
     {
+
         var lossAmt = totalInitPrice - totalCurrentPrice
         var lossPercent = (lossAmt/totalInitPrice) * 100
         output.innerText = `Hey you have a loss of ${lossAmt} and the loss percent is ${lossPercent.toFixed(2)}%`
+        output.className = "out-loss"
     }
     else if(totalCurrentPrice > totalInitPrice){
         var profitAmt = totalCurrentPrice - totalInitPrice
         var profitPercent = (profitAmt/totalInitPrice) * 100
         output.innerText = `Hey you have a profit of ${profitAmt} and the profit percent is ${profitPercent.toFixed(2)}%`
+        output.className = "out-profit"
     }
     else{
         output.innerText = "No pain no gain and no gain no pain"
+        output.className = "output"
     }
 }
 
